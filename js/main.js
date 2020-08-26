3e18344d77e54dd4416f17eba992aa8725389666
Kishore <kishore.nagalingam@ziffity.com>
2020-08-26 14:02:16 +0530

    Added integraios UI

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
-    
-    
+
+    new PerfectScrollbar('.navigation');
+    new PerfectScrollbar('.navigation .submenu');
+
     $('.preloader').addClass('active');
     $(window).load(function() {
-        $('.preloader').delay(5000).fadeOut(function(){
-            $('.img-info, .main-image').delay(6000).addClass('active');
-            $('html').addClass('page-loaded');
-        });
+        $('.img-info, .main-image').delay(6000).addClass('active');
+        function preloader(){
+            $('.preloader').delay(5000).fadeOut(function(){
+                $('.img-info, .main-image').delay(6000).addClass('active');
+                $('html').addClass('page-loaded');
+            });
+        }
+
+        /* if ( !sessionStorage.getItem( 'doNotShow' ) ) {
+            sessionStorage.setItem( 'doNotShow', true );
+            preloader();
+        } else {
+           $ ('.preloader').hide();
+        } */
+    });
+
+    $('#accordion .check-toggle').on("click", function(e) {
+        e.stopPropagation();
     });
 
     $('.parent > a').on('click', function(e){
@@ -17,15 +33,20 @@ $(document).ready(function() {
         $(this).parent().siblings('.parent').removeClass('show');
         $(this).parent().siblings().find('.parent').removeClass('show');
         e.preventDefault();
+
+        if($(this).parent().hasClass('show') == 0) {
+            $(this).parent('.parent').find('.parent').removeClass('show');
+            $(this).parent('.parent').find('.parent .submenu').slideUp();
+        }
     })
 
 
     $('.sidebar, .menu-toggle').on("click", function(e) {
-       // e.stopPropagation();
+        //e.stopPropagation();
     });
 
     $(document).click(function() {
-       // $("body").removeClass("menu-slide")
+       //$("body").removeClass("menu-slide")
     });
 
     $('.menu-toggle').on('click', function(){
@@ -34,8 +55,9 @@ $(document).ready(function() {
         $('.navbar-nav .parent').removeClass('show');
     });
 
-    $('[data-toggle="modal"]').on('click', function(){
+    $('[data-toggle="modal"]').on('click', function(e){
         $("body").removeClass("menu-slide");
+        e.preventDefault();
     })
 
     var winW = $(window).width();
